94a07d4028be5cae3f1a8610b2669679a3fa5c5e
ryan kleffman <ryankleffman47@gmail.com>
2023-07-03 15:04:38 -1000

    start of netdiag & improvements

--- dev/qs-netdiag.py
+++ dev/qs-netdiag.py
@@ -3,6 +3,7 @@ import socket
 import argparse
 #from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, wait
 import time
+from scapy.all import *
 
 error_block = "[!]" # presents option to contiue
 operation_block = "[*]" # standard operation
@@ -26,10 +27,25 @@ def execution_time(func):
     return wrapper
 
 class NetDiag:
-    def __init__(self):
+    def __init__(self, debug):
         '''if not self._valuecheck(ARGS):
             exit()'''
-        pass
+        
+        self.debug = debug
+        
+        self.local_ip = ""
+        self.hostname = ""
+        self.gateway = ""
+        
+        #IP's to ping. Usually gateway, loopback, and any others
+        self.icmp_ip = ["127.0.0.1", self.gateway, self.local_ip]
+        
+        ## domains to look up
+        # 3 seperate domains, note, netflix may be blocked
+        self.dns_domain = ["google.com","netflix.com","microsoft.com"]
+        ## servers to perform the lookups
+        #google, quaddns, clouflare
+        self.dns_servers = ["8.8.8.8","9.9.9.9","1.1.1.1"]
 
     def __str__(self):
         return "=== Details ==="
@@ -53,32 +69,98 @@ class NetDiag:
 
     def main(self):
         #mockup
-        self._dns_lookup()
+        dns_passed = 0
+        dns_total = 0
+        icmp_passed = 0 
+        icmp_total = 0
+
+        #dns
+        for dns_server in self.dns_servers:
+            for dns_domain in self.dns_domain:
+                dns_total = dns_total + 1
+                if self._dns_lookup(domain = dns_domain, dns_server=dns_server, debug = self.debug):
+                    dns_passed = dns_passed + 1
+        print("{} DNS: {}/{} ({:.2f}%) tests passed.".format(operation_block, dns_passed, dns_total, (dns_passed/dns_total) * 100))
+
         self._dns_lookup_reverse()
-        self._icmp_gateway()
+
+        ## ICMP
+        for icmp_ip in self.icmp_ip:
+            self._icmp_gateway(ip=icmp_ip, debug = self.debug)
+
+        print("{} ICMP: {}/{} ({:.2f}%) tests passed.".format(operation_block, dns_passed, dns_total, (dns_passed/dns_total) * 100))
+
+
         self._system_net_info()
 
         print("DNS: OK | GATEWAY: reachable | Other: OK | Other: FAIL")
 
 
-    def _dns_lookup(self):
-        print("{} DNS Query 8.8.8.8 -> google.com Successful".format(operation_block))
-        pass
+    def _dns_lookup(self, dns_server, domain, debug=False):
+        ## chatgpt modified mockup - NEED to format these into correct tabs n stuff
+        # Craft a DNS query packet
+        try:
+            dns_query = IP(dst=dns_server) / UDP() / DNS(rd=1, qd=DNSQR(qname=domain))
+
+            # Send the packet and capture the response
+            response = sr1(dns_query, verbose=False)
+
+            # Check if a response was received
+            if response:
+                # Extract the resolved IP address(es) from the response
+                answers = response[DNS].an
+                if answers:
+                    for answer in answers:
+                        if answer.type == 1:  # Only consider A records
+                            print("{} DNS Query {} -> {}: {}".format(operation_block, domain, dns_server, answer.rdata)) if debug else None
+                            return True
+                            ## do a normal lookup as well if this is successful
+                else:
+                    print("{} DNS Query {} -> {}: No Answers receieved".format(error_block, domain, dns_server)) if debug else None
+
+            else:
+                print("{} DNS Query {} -> {}: No Response receieved".format(error_block, domain, dns_server)) if debug else None
+
+        except Exception as e:
+            print("{} DNS error: {}".format(unrecov_error_block, e))
+
+        #print("{} DNS Query 8.8.8.8 -> google.com Successful".format(operation_block))
+        #pass
 
     def _dns_lookup_reverse(self):
-        print("{} DNS Query google.com -> 8.8.8.8 Successful".format(operation_block))
+        #print("{} DNS Query google.com -> 8.8.8.8 Successful".format(operation_block))
 
         pass
 
-    def _icmp_gateway(self):
-        print("{} ICMP Message -> 192.168.0.1 [GATEWAY] successful".format(operation_block))
+    def _icmp_gateway(self, ip, debug=False):
+        ## being a pain with responsese. need to tesetk
+        try:
+            icmp_request = IP(dst=ip)/ICMP()
 
-        pass
+            # Send the packet and capture the response
+            # using sr1 as we are only getting one response from each I think.
+            result = sr1(icmp_request, timeout=1, verbose=False)
+            
+            if result is not None:
+                #print(f"ICMP response received from {result[IP].src}")
+                print("IP: {}\tPKT: {}".format(result[IP].src, result.summary()))
+
+            else:
+                #print(result)
+                pass
+
+        except Exception as e:
+            print("{} ICMP error occured: {}".format(error_block, e)) if debug else None
+
+        #print("{} ICMP Message -> 192.168.0.1 [GATEWAY] successful".format(operation_block))
+
+        #pass
 
     def _system_net_info(self):
-        print("{} IP: 127.0.0.1 MAC: 00:00:00:00:00:00 Interface: eth0".format(operation_block))
-        pass
+        self.local_ip = socket.gethostbyname(socket.gethostname())
+        self.hostname = socket.gethostname()
 
+        #print("{} IP: {} HOST: {} MAC: {} Interface: {}".format(operation_block, self.local_ip, self.hostname, "__", "__"))
 
 class QsUtils:
     '''
@@ -131,6 +213,8 @@ if __name__ == "__main__":
     #target prolly not needed
     #parser.add_argument('-t', '--target', help="The target you wish to scan. Can be an IP, FQDN, or hostname. Example: 'qs-portscan -t 192.168.0.1'", required=True) 
     parser.add_argument('-d', '--debug', help="Print debug information", action="store_true") 
+    parser.add_argument('-z', help="Print debug information", action="store_true") 
+
     #interface arg, to choose which interface to use
 
     args = parser.parse_args()
@@ -142,6 +226,7 @@ if __name__ == "__main__":
         exit()
 
     netdiag = NetDiag(
+        debug = args.debug
     )
 
     # Logic tree
